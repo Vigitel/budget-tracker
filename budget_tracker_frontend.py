eefcf654f8b87fae929bcdd6e7a44b53df390061
Vigitel <mynameisjeph13@gmail.com>
2024-10-22 06:11:10 +0800

    Update budget_tracker_frontend.py

--- budget_tracker_frontend.py
+++ budget_tracker_frontend.py
@@ -1,29 +1,83 @@
 import tkinter as tk
+from tkinter import ttk
 
-root = tk.Tk()
-root.title("Budget Tracker")
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Budget Tracker")
 
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 4)
+        self.rowconfigure(0, weight = 1)
+
+app = Application()
+
+class InputForm(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+
+
+app.mainloop()
+
+
+'''
 def add_to_list(event=None):
     text = entry.get()
     if text:
        text_list.insert(tk.END, text)  
        entry.delete(0, tk.END)   
 
-frame = tk.Frame(root)
-frame.grid(row = 0, column = 0)
 
-entry = tk.Entry(frame)
-entry.grid(row = 0, column = 0)
+
+
+#weight determines how much percent of additional space the widget will take up 
+#also dictates whether it changes in size when you resize the tab
+
+frame_1 = ttk.Frame(root)
+frame_1.grid(row = 0, column = 0, sticky = "nsew", padx = 5, pady = 5)
+
+frame_1.columnconfigure(0, weight = 1)
+frame_1.rowconfigure(1, weight = 1)
+
+entry = ttk.Entry(frame_1)
+entry.grid(row = 0, column = 0, sticky = "ew")
 
 entry.bind("<Return>",add_to_list)
 
-entry_button = tk.Button(frame, text = "Enter", command=add_to_list)
-entry_button.grid(row = 0, column = 1)
+entry_button_1 = ttk.Button(frame_1, text = "Enter", command=add_to_list)
+entry_button_1.grid(row = 0, column = 1)
 
-text_list = tk.Listbox(frame)
-text_list.grid(row=1,  column=0, columnspan=2, sticky="ew")
+text_list = tk.Listbox(frame_1)
+text_list.grid(row=1,  column=0, columnspan=2, sticky="nsew")
 
 #columnspan is amount of columns it takes, "ew" in sticky means it sticks to east and west
+
+
+
+
+frame_2 = ttk.Frame(root)
+frame_2.grid(row = 0, column = 1, sticky = "nsew", padx = 5, pady = 5)
+
+frame_2.columnconfigure(0, weight = 1)
+frame_2.rowconfigure(1, weight = 1)
+
+entry = ttk.Entry(frame_2)
+entry.grid(row = 0, column = 0, sticky = "ew")
+
+entry.bind("<Return>",add_to_list)
+
+entry_button_2 = ttk.Button(frame_2, text = "Enter", command=add_to_list)
+entry_button_2.grid(row = 0, column = 1)
+
+text_list = tk.Listbox(frame_2)
+text_list.grid(row=1,  column=0, columnspan=2, sticky="nsew")
+'''
+
+
+
+
+
 
 
 '''
