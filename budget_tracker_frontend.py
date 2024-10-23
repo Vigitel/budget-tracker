61cc3a3d1396c8f6f2953a9e9d305455a1fa021a
Vigitel <mynameisjeph13@gmail.com>
2024-10-23 16:56:16 +0800

    Update budget_tracker_frontend.py

--- budget_tracker_frontend.py
+++ budget_tracker_frontend.py
@@ -1,32 +1,66 @@
 import tkinter as tk
 from tkinter import ttk
 
+def main():
+    app = Application()
+    app.mainloop()
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Budget Tracker")
 
-        self.columnconfigure(0, weight = 1)
-        self.columnconfigure(1, weight = 4)
+        
+       
         self.rowconfigure(0, weight = 1)
 
-app = Application()
+        frame1 = InputForm(self)
+        frame1.grid(row = 0, column = 0, sticky = "nsew", padx = 5, pady = 5)
+        self.columnconfigure(0, weight = 1)
+
+
+        frame2 = InputForm(self)
+        frame2.grid(row = 0, column = 1, sticky = "nsew", padx = 5, pady = 5)
+        self.columnconfigure(1, weight = 4)
+       
 
 class InputForm(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.columnconfigure(0, weight = 1)
+        self.rowconfigure(1, weight = 1)
+
+        self.entry = ttk.Entry(self)
+        self.entry.grid(row = 0, column = 0, sticky = "ew")
+
+        self.entry.bind("<Return>",self.add_to_list)
+
+        self.entry_button_1 = ttk.Button(self, text = "Enter", command=self.add_to_list)
+        self.entry_button_1.grid(row = 0, column = 1)
+
+        self.text_list = tk.Listbox(self)
+        self.text_list.grid(row=1,  column=0, columnspan=2, sticky="nsew")
+
+        #columnspan is amount of columns it takes, "ew" in sticky means it sticks to east and west
+
+    def add_to_list(self, event=None):
+    
+        text = self.entry.get()
+        if text:
+            self.text_list.insert(tk.END, text)  
+            self.entry.delete(0, tk.END) 
 
 
-app.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 '''
-def add_to_list(event=None):
-    text = entry.get()
-    if text:
-       text_list.insert(tk.END, text)  
-       entry.delete(0, tk.END)   
+ 
 
 
 
@@ -35,23 +69,8 @@ def add_to_list(event=None):
 #also dictates whether it changes in size when you resize the tab
 
 frame_1 = ttk.Frame(root)
-frame_1.grid(row = 0, column = 0, sticky = "nsew", padx = 5, pady = 5)
 
-frame_1.columnconfigure(0, weight = 1)
-frame_1.rowconfigure(1, weight = 1)
 
-entry = ttk.Entry(frame_1)
-entry.grid(row = 0, column = 0, sticky = "ew")
-
-entry.bind("<Return>",add_to_list)
-
-entry_button_1 = ttk.Button(frame_1, text = "Enter", command=add_to_list)
-entry_button_1.grid(row = 0, column = 1)
-
-text_list = tk.Listbox(frame_1)
-text_list.grid(row=1,  column=0, columnspan=2, sticky="nsew")
-
-#columnspan is amount of columns it takes, "ew" in sticky means it sticks to east and west
 
 
 
@@ -90,4 +109,4 @@ test_label.grid(row = 0, column = 0)
 test_button = tk.Button(root, text = "Buttton 1", command = test_command)
 test_button.grid(row = 0, column = 1)
 '''
-root.mainloop()
+#root.mainloop()
