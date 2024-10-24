4b5dbade35acc59516ee8575bb6f2b9687bbf78a
Vigitel <mynameisjeph13@gmail.com>
2024-10-25 06:59:46 +0800

    Update budget_tracker_backend.py

--- budget_tracker_backend.py
+++ budget_tracker_backend.py
@@ -1,3 +1,11 @@
+from datetime import date
+
 class transcaction():
-    def __init__(self, amount, date, ):
-        pass
+    def __init__(self, type, name, amount, date_of_transaction, tags):
+        self.type = ""
+        self.name = ""
+        self.amount = float()
+        self.date_of_transaction = ""
+        self.tags = []
+
+
